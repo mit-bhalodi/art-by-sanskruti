0a9ac040836d33fde3fefc214bae9f2c81b2b1ad
Meet Bhalodi <meetbhalodi@admins-MacBook-Pro.local>
2023-10-08 12:28:39 +0530

    Art by sanskruti site | angular.json bundle size changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig: {
+        apiKey: 'AIzaSyAKBJ5yd5Cu8B7QLmDxv3JtsPJH2_aLe_g',
+        authDomain: 'silken-fortress-377407.firebaseapp.com',
+        databaseURL: 'https://silken-fortress-377407-default-rtdb.firebaseio.com',
+        projectId: 'silken-fortress-377407',
+        storageBucket: 'silken-fortress-377407.appspot.com',
+        messagingSenderId: '330040351024',
+        appId: '1:330040351024:web:38a7c7f2ecf99ce72450f5',
+        measurementId: 'G-BQZG60D30G',
+    },
 };
