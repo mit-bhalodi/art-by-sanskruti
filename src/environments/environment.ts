1a0e2dba2de133053a59b49fe0fbfc60beaf0f82
Meet Bhalodi <meet.bhalodi@searce.com>
2024-02-04 18:29:45 +0530

    environment file changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    api: 'http://localhost:8000/api',
+    // api: 'http://localhost:8000/api',
+    api: 'https://art-by-sanskruti-backend-production.up.railway.app/api',
 };
