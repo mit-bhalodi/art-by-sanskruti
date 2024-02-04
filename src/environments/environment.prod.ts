fd6b13855584927e6a503a0ce442ad502a763235
Meet Bhalodi <meet.bhalodi@searce.com>
2024-02-04 13:53:26 +0530

    environment file changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    api: 'http://localhost:8000/api',
+    // api: 'http://localhost:8000/api',
+    api: 'https://art-by-sanskruti-backend.vercel.app/',
 };
