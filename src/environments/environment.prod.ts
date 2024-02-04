0ca9a3de29533741710024e005a81891a151d3b6
Meet Bhalodi <meet.bhalodi@searce.com>
2024-02-04 12:22:41 +0530

    environment file changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,4 @@
 export const environment = {
     production: true,
-    firebaseConfig: {
-        apiKey: 'AIzaSyAKBJ5yd5Cu8B7QLmDxv3JtsPJH2_aLe_g',
-        authDomain: 'silken-fortress-377407.firebaseapp.com',
-        databaseURL: 'https://silken-fortress-377407-default-rtdb.firebaseio.com',
-        projectId: 'silken-fortress-377407',
-        storageBucket: 'silken-fortress-377407.appspot.com',
-        messagingSenderId: '330040351024',
-        appId: '1:330040351024:web:e5f684c343f726112450f5',
-        measurementId: 'G-JXQGBFEDXG',
-    },
+    api: 'http://localhost:8000/api',
 };
