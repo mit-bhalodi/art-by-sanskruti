5f877a024da47b8b15438337b29a4840d062a865
Meet Bhalodi <meetbhalodi@Meets-MacBook-Air.local>
2024-02-24 16:09:03 +0530

    updated git ignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-        RAZOR_PAY_API_KEY: 'sdfsfs',
-        production: true,
-        api: 'https://localhost:8000/api'
-    };
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-        RAZOR_PAY_API_KEY: 'sdfsfs',
-        production: false,
-        api: 'http://localhost:8000/api',
-    };
